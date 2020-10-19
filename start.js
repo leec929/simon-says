0fe3258a80679f9a358526d4da97b0744820404e
leec929 <46377936+leec929@users.noreply.github.com>
2020-10-19 11:21:18 -0400

    Update start.js

--- start.js
+++ start.js
@@ -12,7 +12,7 @@ addEventListener('keydown', function(e) {
         if(instr.style.visibility != "hidden") {
             instr.style.visibility = "hidden";
             instr.style.opacity = "0";
-            console.log('closing instr')
+//             console.log('closing instr')
         }
     }
 });
@@ -26,6 +26,5 @@ function toggleInstructions() {
         instructions.style.visibility = "hidden";
         instructions.style.opacity = '0';
     }
-    // visibility sounds nice https://stackoverflow.com/questions/40446658/javascript-add-transition-between-displaynone-and-displayblock
-    // if visible, make it none - and vice versa.
+    // visibility reference https://stackoverflow.com/questions/40446658/javascript-add-transition-between-displaynone-and-displayblock
 }
