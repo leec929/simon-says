bee52065be2f9531d2109b77dc40bdd24aa4b5b9
leec929 <46377936+leec929@users.noreply.github.com>
2020-10-20 10:44:02 -0400

    leaderboard redirect to comet

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 document.getElementById("how-button").addEventListener("click", toggleInstructions)
 document.getElementById("close-button").addEventListener("click", toggleInstructions)
-document.getElementById("leader-button").addEventListener("click", function() {location.href="./leaderboard.php";})
+document.getElementById("leader-button").addEventListener("click", function() {location.href="http://comet.cs.brynmawr.edu/~cwlee/cs380-projects/leaderboard.php";})
 document.getElementById("start-button").addEventListener("click", function() { location.href = "./game.html"; })
 
 addEventListener('keydown', function(e) {
