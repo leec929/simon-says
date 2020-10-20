faa4bcf28cb146b99e6be666ffd2c0d953217820
leec929 <46377936+leec929@users.noreply.github.com>
2020-10-20 10:16:33 -0400

    add hidden input fields and clean up php

--- submit-score.php
+++ submit-score.php
@@ -1,4 +1,4 @@
-<!-- TODO: Make game.html make a POST request with a score field -->
+<!-- TODO: Make game.html make a POST request with "score" and "date" (or primary id) inputs -->
 <!-- TODO: Style with CSS -->
 
 <!DOCTYPE html>
@@ -14,29 +14,27 @@
                 echo <<<LONG
         <h2 style="color: #d4341c;"> No score available! </h2>
         <p> Did you get to this page from playing the game? </p>
-        <button type="button">Return to Main Page</button>
 LONG;
             }
             else {
+                echo "<h1> Type post[score] is " . gettype($_POST["score"]);
                 echo <<<LONG
         <h2> Enter your initials </h2>
         <form id="initial-wrapper" action="./leaderboard.php" method="POST">
             <input type="text" maxlength="1" placeholder="-" id="first-initial">
             <input type="text" maxlength="1" placeholder="-">
             <input type="text" maxlength="1" placeholder="-">
-        </form>
-        <h2> Your Score </h2>
-        <p id="score">
 LONG;
-                echo $_POST["score"];
-                echo <<<LONG
-</p>
-        <button type="submit" form="initial-wrapper">Confirm</button>
-        <button type="button" id="return-btn">Return to Main Page</button>
-        <script src="./submit.js"></script>
-LONG;
+                echo "            <input type=\"hidden\" name=\"score\" value=\"" . $_POST["score"] . "\">";
+                // TODO: uncomment when date is sent
+                //echo "<input type=\"hidden\" name=\"date\" value=\"" . $_POST["date"] . "\">";
+                echo "        </form>";
+                echo "        <h2> Your Score </h2>";
+                echo "        <p>" . $_POST["score"] . "</p>";
+                echo "        <button type=\"submit\" form=\"initial-wrapper\">Confirm</button>"
             }
         ?>
-        
+        <button type="button" id="return-btn">Return to Main Page</button>
+        <script src="./submit.js"></script>
     </body>
 </html>
