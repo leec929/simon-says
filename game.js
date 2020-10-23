1fbb2c6cfac404712f76d3ce2f7e787db98a9549
leec929 <46377936+leec929@users.noreply.github.com>
2020-10-23 15:18:06 -0400

    submit logic finished

--- game.js
+++ game.js
@@ -5,21 +5,38 @@ var userPattern = [];
 
 var started = false;
 var level = 0;
+var score = 0;
 
-$(document).ready(aspectButtons); // make the button height be equal to width at startup regardless of device
+var wait = 0; // to disable click event handler while nextsequence() is happening.
 
-$(document).keypress(function() {
+$(document).ready( function () {
+    $("#yourscore").hide();
+    $("#submitform").attr("action", "http://comet.cs.brynmawr.edu/~cwlee/cs380-projects/submit-score.php");
+    aspectButtons();
+}); // make the button height be equal to width at startup regardless of device
+
+$(document).keypress(function() { startGame(); })
+
+$("#playbtn").click(function() {
+    startGame();
+});
+
+function startGame() {
     $("#endGame1").text("");
     $("#endGame2").text("");
-    if (!started) {
-        $("#level-title").text("Level " + level);
-        nextSequence();
-        started = true;
-    }
-})
+    $("#yourscore").hide();
+    $("#level-title").text("Level " + level);
+    started = true;
+    $(".utilbtn").hide();
+    $("#playbtn").text("Restart");
+    // move these up in case user presses a wrong button within the timeout of nextSequence()
+    level = 0;
+    gamePattern = [];
+    nextSequence();
+}
 
 $(".btn").click(function() {
-    if(started) {
+    if(started && !wait) {
         var userChosenColor = $(this).attr("id");
         userPattern.push(userChosenColor);
 
@@ -31,12 +48,14 @@ $(".btn").click(function() {
 })
 
 function checkAnswer(currentLevel) {
+    wait = 1;
     if (gamePattern[currentLevel] === userPattern[currentLevel]) {
         if (userPattern.length === gamePattern.length){
             setTimeout(function() {
                 nextSequence();
             }, 1000);
-        }
+        } else
+            wait = 0;
     } else{
         playSound("wrong");
         $("body").addClass("game-over");
@@ -48,7 +67,6 @@ function checkAnswer(currentLevel) {
             $("body").removeClass("game-over");
         }, 200);
 
-        // document.addEventListener() // add keydown event listener which should be removed in startover.
         startOver();
     }
 }
@@ -63,6 +81,7 @@ function nextSequence() {
 
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColor);
+    wait = 0;
 }
 
 function animatePress(currentColor) {
@@ -78,9 +97,11 @@ function playSound(name) {
 }
 
 function startOver() {
-    level = 0;
-    gamePattern = [];
     started = false;
+    score = level - 1;
+    $("#yourscore").text("Your score: " + score);
+    $("#yourscore").show();
+    $(".utilbtn").show();
 }
 
 
@@ -101,3 +122,16 @@ function aspectButtons() {
 }
 // on window resize, to make the buttons 1:1 aspect ratio
 window.addEventListener('resize', aspectButtons); 
+
+function getTwoDigits(num) { return ("0" + num).slice(-2); }
+function getMySQLTime() {
+    let now = new Date();
+    return `${now.getFullYear()}-${getTwoDigits(now.getMonth()+1)}-${getTwoDigits(now.getDate())} ${getTwoDigits(now.getHours())}:${getTwoDigits(now.getMinutes())}:${getTwoDigits(now.getSeconds())}`;
+}
+
+$("#submitbtn").click(function() {
+    $("#score").val(score);
+    let now = new Date();
+    $("#playdatetime").val(getMySQLTime());
+    $("#submitform").submit();
+});
