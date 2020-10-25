de3dc3c8d0abaa4b5fc2c97d6720e8b18a6a5465
leec929 <46377936+leec929@users.noreply.github.com>
2020-10-25 11:46:21 -0400

    get rid of unused var

--- game.js
+++ game.js
@@ -170,7 +170,6 @@ function getMySQLTime() {
  */
 $("#submitbtn").click(function() {
     $("#score").val(score);
-    let now = new Date();
     $("#playdatetime").val(getMySQLTime());
     $("#submitform").submit();
 });
